0c6c0b5cf8ad6ed0266df5e871d8a0e01aa01334
rebelliousi <learneryolo3@gmail.com>
2026-06-30 15:18:03 +0500

    feat: add button and handle with running state

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,27 @@ import { useEffect, useState } from "react"
 const App=()=>{
     const [second,setSecond]=useState(0)
 
+    const [isRunning,setIsRunning]=useState(false)
+
     useEffect(()=>{
+
+        if (isRunning === false)
+            return
         const interval=setInterval(()=>{
             setSecond(prev=>prev+1)
         },1000)
 
         return()=> clearInterval(interval)
-    }) 
+    },[isRunning]) 
+
+    const handleRunning=()=>{
+        setIsRunning(prev=>!prev)
+    }
     return(
         <div>
           <h1>{second}</h1>
+
+          <button  onClick={handleRunning}>start</button>
         </div>
     )
 }
