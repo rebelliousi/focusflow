92dd75db8b891a8597915c739c82904270cae052
rebelliousi <learneryolo3@gmail.com>
2026-06-30 15:04:30 +0500

    feat:make it add auto with setInterval -auto repeater

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
-const App=()=>()
+import { useEffect, useState } from "react"
+
+const App=()=>{
+    const [second,setSecond]=useState(0)
+
+    useEffect(()=>{
+        const interval=setInterval(()=>{
+            setSecond(prev=>prev+1)
+        },1000)
+
+        return()=> clearInterval(interval)
+    }) 
+    return(
+        <div>
+          <h1>{second}</h1>
+        </div>
+    )
+}
+
+export default App
